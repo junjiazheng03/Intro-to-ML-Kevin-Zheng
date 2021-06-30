d3cffff566df9567ea78482b54f16aafaa09b194
kevin-zheng0 <86612288+kevin-zheng0@users.noreply.github.com>
2021-06-30 01:53:02 -0400

    Create Paths for Values over Days.R

---
+++ Paths for Values over Days.R
@@ -0,0 +1,22 @@
+newFCT2 = function(L = 100, P = 10) { ## L = Length of the simulation, P = Number of paths
+  ##Next step: use data.frame to return full data if requested
+  if (is.numeric(L) == TRUE & is.numeric(P) == TRUE) { ## Check if inputs are valid
+    plot(rnorm(L, 0.000, 0.025), type = "l", ylim = c(0, 3), xlab = "Number of Days", 
+         ylab = "Expected Value", col="white")
+    for (i in 1:P) { ## Repeat for each path
+      data1 = rnorm(L, 0.000, 0.025)
+      data2 = data1 + 1
+      data3 = cumprod(data2)
+      x <- seq(1,L,1)
+      y <- data3
+      lines(x, y, col="black", type="l")
+    }
+  }
+  else {
+    data3 = "404 error"
+  }
+  ##return(list(finaldata = data3))
+}
+
+tmpHolder = newFCT2(L = 200, P=15)
+##tmpHolder$finaldata
